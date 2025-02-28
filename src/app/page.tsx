661130dd217ee580965067d7df8c46317815ef21
Yubaraj Biswas <ybtheflash@gmail.com>
2025-02-28 08:37:30 +0530

    NOW ROOT POYO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+export const dynamic = 'force-dynamic';
 import { useEffect, useState } from 'react';
 import HeroSection from '@/components/ui/HeroSection';
 import FeaturesSection from '@/components/ui/FeaturesSection';
