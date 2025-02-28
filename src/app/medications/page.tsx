3d23bc3bfa10534565fc6c86075445fd25eaf05e
Sourish Bose <sourish.cse121120@bppimt.ac.in>
2025-02-28 05:31:04 +0530

    fixed medkit

--- src/app/medications/page.tsx
+++ src/app/medications/page.tsx
@@ -1,14 +1,12 @@
 "use client"
 
 import PageWrapper from '@/components/PageWrapper';
-import AddMedicationForm from '@/components/AddMedicationForm';
 import RenderAllMedications from '@/components/getAllMedications';
 
 const MedicationsPage = () => {
   return (
     <PageWrapper>
       <div>
-        <AddMedicationForm />
         <RenderAllMedications />
       </div>
     </PageWrapper>
