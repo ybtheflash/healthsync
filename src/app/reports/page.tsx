4433ba4df8c719edc5c4ce312b2858d49239761c
Yubaraj Biswas <ybtheflash@gmail.com>
2025-02-28 09:15:38 +0530

    hello

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,7 +1,7 @@
 export const dynamic = 'force-dynamic';
 import PageWrapper from '@/components/PageWrapper';
 import ReportsPage from '@/components/ReportsPage';
-export default function ProfilePage() {
+export default function Reports() {
   return (
     <PageWrapper>
       <ReportsPage />
