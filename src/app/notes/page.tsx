392dfe756c82d2547c46e5409f7d3cfd3b6c81bc
Yubaraj Biswas <ybtheflash@gmail.com>
2025-02-28 08:32:35 +0530

    poyo

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic';
 "use client";
+export const dynamic = 'force-dynamic';
 
 import { useState, useEffect, useCallback } from "react";
 import { Client, Storage, Databases, ID } from "appwrite";
